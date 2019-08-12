abeeb71b65aa54e7868911ab7e09062d4072d99b
wiggens <306056160@qq.com>
2019-08-12 18:01:33 +0800

    清理zilongshanren-programmint文件夹

--- layers/zilongshanren-programming/funcs.el
+++ layers/zilongshanren-programming/funcs.el
@@ -22,8 +22,6 @@ comment box."
     (goto-char e)
     (set-marker e nil)))
 
-
-
 ;; "http://stackoverflow.com/questions/2242572/emacs-todo-indicator-at-left-side"
 (defun zilongshanren/annotate-todo ()
   "put fringe marker on TODO: lines in the curent buffer"
@@ -34,15 +32,6 @@ comment box."
       (let ((overlay (make-overlay (- (point) 5) (point))))
         (overlay-put overlay 'before-string (propertize "A"
                                                         'display '(left-fringe right-triangle)))))))
-
-
-;;js2-mode enhancement
-(defun zilongshanren/js2-which-function ()
-  ;; clean the imenu cache
-  ;; @see http://stackoverflow.com/questions/13426564/how-to-force-a-rescan-in-imenu-by-a-function
-  (setq imenu--index-alist nil)
-  (which-function-mode t)
-  (which-function))
 
 (defun zilongshanren/run-current-file ()
   "Execute the current file.
@@ -90,33 +79,6 @@ version 2015-08-21"
             (async-shell-command ξcmd-str "*zilongshanren/run-current-file output*"))
         (message "No recognized program file suffix for this file.")))))
 
-
-
-(defun my-web-mode-indent-setup ()
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
-
-(defun my-toggle-web-indent ()
-  (interactive)
-  ;; web development
-  (if (eq major-mode 'json-mode)
-      (progn
-        (setq js-indent-level (if (= js-indent-level 2) 4 2))))
-
-  (if (or (eq major-mode 'js-mode) (eq major-mode 'js2-mode))
-      (progn
-        (setq js-indent-level (if (= js-indent-level 2) 4 2))))
-
-  (if (eq major-mode 'web-mode)
-      (progn (setq web-mode-markup-indent-offset (if (= web-mode-markup-indent-offset 2) 4 2))
-             (setq web-mode-css-indent-offset (if (= web-mode-css-indent-offset 2) 4 2))
-             (setq web-mode-code-indent-offset (if (= web-mode-code-indent-offset 2) 4 2))))
-  (if (eq major-mode 'css-mode)
-      (setq css-indent-offset (if (= css-indent-offset 2) 4 2)))
-
-  (setq indent-tabs-mode nil))
-
 (defun zilongshanren/load-yasnippet ()
   (interactive)
   (unless yas-global-mode
@@ -128,92 +90,11 @@ version 2015-08-21"
       (setq yas-wrap-around-region t)))
   (yas-minor-mode 1))
 
-(defun conditionally-enable-lispy ()
-  (when (eq this-command 'eval-expression)
-    (lispy-mode 1)))
-
-(defun cmake-rename-buffer ()
-  "Renames a CMakeLists.txt buffer to cmake-<directory name>."
-  (interactive)
-  (when (and (buffer-file-name)
-             (string-match "CMakeLists.txt" (buffer-name)))
-    (setq parent-dir (file-name-nondirectory
-                      (directory-file-name
-                       (file-name-directory (buffer-file-name)))))
-    (setq new-buffer-name (concat "cmake-" parent-dir))
-    (rename-buffer new-buffer-name t)))
-
-(defun zilongshanren/impatient-mode-hook ()
-  "my web mode hook for HTML REPL"
-  (interactive)
-  (impatient-mode)
-  (spacemacs|hide-lighter impatient-mode)
-  (httpd-start))
-
-(defun my-js2-mode-hook ()
-  (progn
-    (define-key js2-mode-map "\C-ci" 'my-js-doc-insert-function-doc-snippet)
-    (define-key js2-mode-map "@" 'js-doc-insert-tag)
-    (modify-syntax-entry ?_ "w")
-    (which-function-mode t)
-    (setq imenu-create-index-function 'js2-imenu-make-index)
-
-    (setq mode-name "JS2")
-    (define-key js2-mode-map   (kbd "s-.") 'company-tern)
-    (spacemacs/toggle-syntax-checking-on)
-    (setq forward-sexp-function nil)
-    (set (make-local-variable 'semantic-mode) nil)))
-
-(defun my-which-function ()
-  ;; clean the imenu cache
-  ;; @see http://stackoverflow.com/questions/13426564/how-to-force-a-rescan-in-imenu-by-a-function
-  (setq imenu--index-alist nil)
-  (which-function))
-
-(defun js2-imenu-make-index ()
-  (interactive)
-  (save-excursion
-    ;; (setq imenu-generic-expression '((nil "describe\\(\"\\(.+\\)\"" 1)))
-    (imenu--generic-function '(("describe" "\\s-*describe\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-                               ("it" "\\s-*it\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-                               ("test" "\\s-*test\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-                               ("before" "\\s-*before\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-                               ("after" "\\s-*after\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-                               ("Controller" "[. \t]controller([ \t]*['\"]\\([^'\"]+\\)" 1)
-                               ("Controller" "[. \t]controllerAs:[ \t]*['\"]\\([^'\"]+\\)" 1)
-                               ("Filter" "[. \t]filter([ \t]*['\"]\\([^'\"]+\\)" 1)
-                               ("State" "[. \t]state([ \t]*['\"]\\([^'\"]+\\)" 1)
-                               ("Factory" "[. \t]factory([ \t]*['\"]\\([^'\"]+\\)" 1)
-                               ("Service" "[. \t]service([ \t]*['\"]\\([^'\"]+\\)" 1)
-                               ("Module" "[. \t]module([ \t]*['\"]\\([a-zA-Z0-9_\.]+\\)" 1)
-                               ("ngRoute" "[. \t]when(\\(['\"][a-zA-Z0-9_\/]+['\"]\\)" 1)
-                               ("Directive" "[. \t]directive([ \t]*['\"]\\([^'\"]+\\)" 1)
-                               ("Event" "[. \t]\$on([ \t]*['\"]\\([^'\"]+\\)" 1)
-                               ("Config" "[. \t]config([ \t]*function *( *\\([^\)]+\\)" 1)
-                               ("Config" "[. \t]config([ \t]*\\[ *['\"]\\([^'\"]+\\)" 1)
-                               ("OnChange" "[ \t]*\$(['\"]\\([^'\"]*\\)['\"]).*\.change *( *function" 1)
-                               ("OnClick" "[ \t]*\$([ \t]*['\"]\\([^'\"]*\\)['\"]).*\.click *( *function" 1)
-                               ("Watch" "[. \t]\$watch( *['\"]\\([^'\"]+\\)" 1)
-                               ("Function" "function[ \t]+\\([a-zA-Z0-9_$.]+\\)[ \t]*(" 1)
-                               ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
-                               ("Function" "^var[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
-                               ("Function" "^[ \t]*\\([^while|for ][a-zA-Z0-9_$]*\\)[ \t]*([a-zA-Z0-9_$,/\\* ]*)[ \t]*" 1)
-                               ("Function" "^[ \t]*static[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*()[ \t]*{" 1)
-                               ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*:[ \t]*function[ \t]*(" 1)
-                               ("Class" "^[ \t]*var[ \t]*\\([0-9a-zA-Z]+\\)[ \t]*=[ \t]*\\([a-zA-Z]*\\).extend" 1)
-                               ("Class" "^[ \t]*cc\.\\(.+\\)[ \t]*=[ \t]*cc\.\\(.+\\)\.extend" 1)
-                               ("Task" "[. \t]task([ \t]*['\"]\\([^'\"]+\\)" 1)))))
-
-(defun my-doxymacs-font-lock-hook ()
-  (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode))
-      (doxymacs-font-lock)))
-
 (defun my-project-name-contains-substring (REGEX)
   (let ((dir (if (buffer-file-name)
                  (file-name-directory (buffer-file-name))
                "")))
     (string-match-p REGEX dir)))
-
 
 (defvar my-tags-updated-time nil)
 
@@ -252,16 +133,3 @@ version 2015-08-21"
         (setq my-tags-updated-time (current-time))
         (my-update-tags)
         (message "updated tags after %d seconds." (- (float-time (current-time)) (float-time my-tags-updated-time))))))
-
-
-(defun my-setup-develop-environment ()
-  (interactive)
-  (when (my-project-name-contains-substring "guanghui")
-    (cond
-     ((my-project-name-contains-substring "cocos2d-x")
-      ;; C++ project don't need html tags
-      (setq tags-table-list (list (my-create-tags-if-needed "~/cocos2d-x/cocos"))))
-     ((my-project-name-contains-substring "Github/fireball")
-      (message "load tags for fireball engine repo...")
-      ;; html project donot need C++ tags
-      (setq tags-table-list (list (my-create-tags-if-needed "~/Github/fireball/engine/cocos2d")))))))
